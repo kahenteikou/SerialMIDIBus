2b316a6b620443abc19b11c0261ca1296f139d27
kokkiemouse <kokkiemouse@fascode.net>
2021-12-26 18:12:49 +0900

    added

--- SerialMIDIBus/MainWindow.xaml.cs
+++ SerialMIDIBus/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,19 +25,52 @@ namespace SerialMIDIBus
         {
             InitializeComponent();
         }
+        private List<string> comportlist=new List<string>();
+        private void SerialPortRefresh()
+        {
+            int indexNo = 0;
+            SerialPortCombo.Items.Clear();
+            ManagementClass devicekun = new ManagementClass("Win32_PnPEntity");
+            foreach(ManagementObject prt in devicekun.GetInstances())
+            {
+                string ptrname = (string)prt.GetPropertyValue("Name");
+                if (ptrname == null) continue;
+                indexNo = ptrname.IndexOf("(COM");
+                if(indexNo >= 0)
+                {
+                    SerialPortCombo.Items.Add(prt.GetPropertyValue("Caption"));
+                    comportlist.Add(ptrname.Substring(indexNo + 1).Replace(")", ""));
+                }
+            }
+        }
         private void midiRefresh()
         {
-
+            MidiComboBox.Items.Clear();
             IEnumerable<W32MIDI.MidiInCaps> midiInCapsEnumerator = W32MIDI.GetmidiList();
             foreach (W32MIDI.MidiInCaps midiCapskun in midiInCapsEnumerator)
             {
-                MessageBox.Show(midiCapskun.szPname);
-                MessageBox.Show(midiCapskun.indexnumber.ToString());
+                MidiComboBox.Items.Add(midiCapskun.szPname);
             }
 
         }
         private void StartBT_Click(object sender, RoutedEventArgs e)
         {
+        }
+
+        private void MIDIRefreshBT_Click(object sender, RoutedEventArgs e)
+        {
+            midiRefresh();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            midiRefresh();
+            SerialPortRefresh();
+        }
+
+        private void SerialPortRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            SerialPortRefresh();
         }
     }
 }
